7e60feb7d32b0f6c9306511e4f75f9d82ac67c37
Eduardo <139073033+educcamara@users.noreply.github.com>
2023-08-02 21:50:07 -0400

    MaiorNum v2
    Apenas com maior2 e maiornum, dando uma lista de inteiros de qualquer tamanho, é possível descobrir o maior número dentre eles.

--- maiornum.hs
+++ maiornum.hs
@@ -3,8 +3,13 @@ maior2 num1 num2
     | num1 > num2 = num1
     | otherwise = num2
 
-maior3 :: Int -> Int -> Int -> Int
-maior3 num1 num2 num3 = maior2 num1 (maior2 num2)
+-- maior3 :: Int -> Int -> Int -> Int
+-- maior3 num1 num2 num3 = maior2 num1 (maior2 num2 num3)
 
-maior5 :: Int -> Int -> Int -> Int -> Int -> Int
-maior5 num1 num2 num3 num4 num5 = maior2 (maior3 num1 num2 num3) (maior2 num4 num5)
+-- maior5 :: Int -> Int -> Int -> Int -> Int -> Int
+-- maior5 num1 num2 num3 num4 num5 = maior2 (maior3 num1 num2 num3) (maior2 num4 num5)
+
+maiornum :: [Int] -> Int
+maiornum lista
+    | length lista == 1 = head lista
+    | otherwise = maior2 (head lista) (maiornum(tail lista))
