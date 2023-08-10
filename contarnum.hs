9a44373359b0c47b5bdc7e9a3ff0fdc9539446c2
Eduardo <139073033+educcamara@users.noreply.github.com>
2023-08-10 18:51:28 -0400

    ContarNum v2
    Agora é capaz de contar apenas números ímpares ou pares.

--- contarnum.hs
+++ contarnum.hs
@@ -3,3 +3,15 @@ contarnum :: [Int] -> Int
 
 contarnum [] = 0
 contarnum (x:xs) = 1 + contarnum xs
+
+contarpares :: [Int] -> Int
+contarpares [] = 0
+contarpares (x:xs)
+    | even x = 1 + contarpares xs
+    | otherwise = contarpares xs
+
+contarimpares :: [Int] -> Int
+contarimpares [] = 0
+contarimpares (x:xs)
+    | odd x = 1 + contarimpares xs
+    | otherwise = contarimpares xs
