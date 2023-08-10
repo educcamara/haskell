7459f232b87556f1cdddf0bf0f78f2308b49acdd
Eduardo <139073033+educcamara@users.noreply.github.com>
2023-08-10 18:19:40 -0400

    MaiorNum v3
    versão mais compactada, sem a necessidade de 2 funções

--- maiornum.hs
+++ maiornum.hs
@@ -1,15 +1,7 @@
-maior2 :: Int -> Int -> Int
-maior2 num1 num2
-    | num1 > num2 = num1
-    | otherwise = num2
-
--- maior3 :: Int -> Int -> Int -> Int
--- maior3 num1 num2 num3 = maior2 num1 (maior2 num2 num3)
-
--- maior5 :: Int -> Int -> Int -> Int -> Int -> Int
--- maior5 num1 num2 num3 num4 num5 = maior2 (maior3 num1 num2 num3) (maior2 num4 num5)
-
 maiornum :: [Int] -> Int
-maiornum lista
-    | length lista == 1 = head lista
-    | otherwise = maior2 (head lista) (maiornum(tail lista))
+
+maiornum [] = error "Não existe número na lista"
+maiornum [x] = x
+maiornum (x:y:xs)
+    | x > y = maiornum (x:xs)
+    | otherwise = maiornum (y:xs)
