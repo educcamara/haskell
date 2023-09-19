776fe43b011420121c3c39ae40178489491a7a69
dudu <eduardo.camara@icomp.ufam.edu.br>
2023-09-19 15:17:21 -0400

    Correção da descrição de funções

--- alta_ordem.hs
+++ alta_ordem.hs
@@ -1,15 +1,19 @@
 dobraD :: (a -> a -> a) -> a -> [a] -> a
 {-\####FUNÇÃO DOBRA PELA DIREITA####
-    Função de Alta Ordem que recebe uma função, valor auxiliar e
-    uma lista que será compactada a um único valor da direita para a esquerda
+        Função(2 valores 'a' -> 'a') 
+    -> valor 'a' base
+    -> lista de 'a's
+    -> Retorna valor compactado do tipo 'a'
 -}
 dobraD func y [x] = func x y
 dobraD func y (x:xs) = func x (dobraD func y xs)
 
 dobraE :: (a -> a -> a) -> a -> [a] -> a
 {-\####FUNÇÃO DOBRA PELA DIREITA####
-    Função de Alta Ordem que recebe uma função, valor auxiliar e
-    uma lista que será compactada a um único valor da esquerda para a direita
+    Função(2 valores 'a' -> 'a') 
+    -> valor 'a' base
+    -> lista de 'a's
+    -> Retorna valor compactado do tipo 'a'
 -}
 dobraE func y [x] = func y x
 dobraE func y (x:xs) = dobraE func (func y x) xs
